c9a799e9aca996549de936e1b02502ba12c208ca
Alfredo Pantoja <chololoy@gmail.com>
2013-09-03 14:14:25 -0500

    Finish people create action

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,4 +2,24 @@ class PeopleController < ApplicationController
   def show
     @person = Person.find(params[:id])
   end  
+
+  def new
+    @person = Person.new
+  end  
+
+  def create
+    @person = Person.new(person_params) 
+    if @person.save
+      flash[:success] = "Welcome to the Contact Manager!"
+      redirect_to @person
+    else 
+      render 'new'
+    end
+  end  
+
+  private 
+
+    def person_params
+      params.require(:person).permit(:first_name, :last_name)
+    end  
 end  
--- spec/requests/person_pages_spec.rb
+++ spec/requests/person_pages_spec.rb
@@ -12,4 +12,36 @@ describe "Person pages" do
     it { should have_content(person.name) }
     it { should have_title(person.name) }
   end    
+
+  describe "new person page" do
+    
+    before { visit new_person_path }
+
+    it { should have_content('Create new person') }
+    it { should have_title(full_title('Create new person')) }
+  end  
+
+  describe "creating a new person" do
+
+    before { visit new_person_path }
+
+    let(:submit) { "Create new person" }
+
+    describe "with invalid information" do
+      it "should not create a person" do
+        expect { click_button submit }.not_to change(Person, :count)
+      end
+    end  
+
+    describe "with valid information" do
+      before do
+        fill_in "First name", with: "Alfredo"
+        fill_in "Last name",  with: "Pantoja"
+      end
+
+      it "should create a person" do
+        expect { click_button submit }.to change(Person, :count).by(1)
+      end
+    end
+  end  
 end
